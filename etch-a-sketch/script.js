46eccf9c1e20274aa3a3f0bd15c024a7b40e57f8
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-29 21:50:12 +0800

    Add more comments

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -4,6 +4,12 @@ const numberOfSquares = n * n
 const square = document.createElement("div")
 square.classList.add("square");
 
+document.getElementById("container").appendChild(square);
+
 //need a loop that creates square elements until number is done.
 //calculate squares per side size to get square size -> add that style to square element
 //then flex wrap
+
+//for button to input number of sq per side, 
+// remove all previous square elements first using innerHTML = "";
+//, run function to create new set of squares
