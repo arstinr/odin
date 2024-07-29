3b22b3fe32ac672d9414d6c008171bdbe2813ba2
Arstin Reano <arstingabrelreano@gmail.com>
2024-07-29 21:24:49 +0800

    Add js comments for algo

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -3,3 +3,7 @@ const n = 16
 const numberOfSquares = n * n
 const square = document.createElement("div")
 square.classList.add("square");
+
+//need a loop that creates square elements until number is done.
+//calculate squares per side size to get square size -> add that style to square element
+//then flex wrap
