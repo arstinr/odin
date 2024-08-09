7ee06bb9fb51b200f791da52a8fc665953159134
Arstin Reano <arstingabrelreano@gmail.com>
2024-08-09 15:36:29 +0800

    Basic numbers

--- Ruby/practice.rb
+++ Ruby/practice.rb
@@ -1,1 +1,21 @@
 ##Practice file
+
+# Addition
+puts 1 + 1
+
+# Subtraction
+puts 2 - 1
+
+# Multiplication
+puts 10 * 0.1
+
+# Division
+puts 10 / 10
+
+# Exponents
+puts 2 ** 2
+puts 3 ** 4
+
+# Modulus (find remainder of division)
+puts 8 % 2 
+puts 10 % 4
