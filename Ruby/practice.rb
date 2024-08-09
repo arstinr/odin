f606ceb69fd4fed894bc6250ff87638ac47737c5
Arstin Reano <arstingabrelreano@gmail.com>
2024-08-09 16:33:47 +0800

    Strings

--- Ruby/practice.rb
+++ Ruby/practice.rb
@@ -19,3 +19,29 @@ puts 3 ** 4
 # Modulus (find remainder of division)
 puts 8 % 2 
 puts 10 % 4
+
+# integers are whole numbers
+# floats are numbers with decimal point
+
+# converting number types
+# to float
+  # .to_f
+# to int
+  # .to_i
+
+# Other methods
+# .even?
+# .odd?
+
+# String concatenations
+
+puts "Hello " + "World!"
+puts "Helow " << "World!"
+
+puts "Hellowww ".concat("World!")
+
+#Substrings (access strings inside strings)
+puts "hello"[0]
+puts "hello"[0..1]
+puts "hello"[0, 4]
+puts "hello"[-1]
