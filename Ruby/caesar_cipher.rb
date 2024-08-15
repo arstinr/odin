b201986acb09c9bb4d7440f7ce25a94887c99734
Arstin Reano <arstingabrelreano@gmail.com>
2024-08-15 14:12:20 +0800

    Create new method

--- Ruby/caesar_cipher.rb
+++ Ruby/caesar_cipher.rb
@@ -13,3 +13,8 @@
       # use lowercase hash
       # get key and then add or subtract n units
       # store new value of new key
+
+def caesar_cipher(text, shift_value)
+  text = gets.chomp
+  shift_value = gets.chomp
+end
