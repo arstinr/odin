e1b6847c8d28def1fdbe8637e499d1fc5062d593
Arstin Reano <arstingabrelreano@gmail.com>
2024-09-14 15:39:03 +0800

    Lessons

---
+++ Ruby/methodpractice.rb
@@ -0,0 +1,14 @@
+def addition(a, b)
+    answer = a + b
+    return answer
+end
+
+def ctof(c)
+    answer = (c * (9.0/5)) + 32
+    return answer
+end
+
+def ftoc(f)
+    answer = (f - 32) * (5.0/9)
+    return answer
+end
